41ead8a5ab74b042cb3d9debee7eb96988acc5b3
lunqwe <roma.boyko10068@gmail.com>
2024-04-19 17:59:29 +0300

    it was no typo :)

--- config/events/backends.py
+++ config/events/backends.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
 
-class AuthBackend(ModelBackend):
+class EmailBackend(ModelBackend):
     def authenticate(self, request, email=None, password=None, **kwargs):
         UserModel = get_user_model()
         try:
